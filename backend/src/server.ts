da6f93f88e7d92bed3a6e84383bf2be9c1fa5675
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2025-11-20 08:55:41 +0100

    woking with backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
-import bookingRoutes from "./src/routers/bookingsRouters";
+import bookingRoutes from "./routers/bookingsRouters";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", bookingRoutes);
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
+
 app.listen(PORT, () => {
-  console.log(`Backend local corriendo en http://localhost:${PORT}/api`);
+  console.log(`Local backend running on http://localhost:${PORT}/api`);
 });
